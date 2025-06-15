dde7c727212aab24b22b37a228ac61a4e8de1025
Chase Wooten <chaserwooten@gmail.com>
2025-06-15 01:14:54 -0600

    Fix login: switch to compat SDKs and expose auth globally

--- public/js/firebase-config.js
+++ public/js/firebase-config.js
@@ -1,25 +1,20 @@
 // public/js/firebase-config.js
 
-// Your Firebase configuration
+// Your Firebase config
 const firebaseConfig = {
   apiKey:            "AIzaSyDHnyLP2EWkPPD3c4JVVGjUphYKASoVEkI",
   authDomain:        "wyrm-collective.firebaseapp.com",
   projectId:         "wyrm-collective",
-  storageBucket:     "wyrm-collective.appspot.com",      // <-- corrected bucket host
+  storageBucket:     "wyrm-collective.appspot.com",
   messagingSenderId: "450404872617",
   appId:             "1:450404872617:web:9644350024fd570d0614b8",
   measurementId:     "G-E9FPEBEMMX"
 };
 
-// Initialize the compat (namespaced) SDK
+// Initialize Firebase compat SDK
 firebase.initializeApp(firebaseConfig);
 
-// Grab the services off the global `firebase`
-const db      = firebase.firestore();
-const auth    = firebase.auth();
-const storage = firebase.storage();
-
-// Make them available globally if you like
-window.db      = db;
-window.auth    = auth;
-window.storage = storage;
+// Expose auth & db globally
+window.auth = firebase.auth();
+window.db   = firebase.firestore();
+window.storage = firebase.storage();
